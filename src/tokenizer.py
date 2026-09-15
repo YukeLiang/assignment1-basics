fcff51c6bcf326fa7352534598450c99d88c8b2c
Yuke Liang <yukeliang@Yukes-MacBook-Air.local>
2026-09-14 23:09:37 -0700

    add pre-tokenization

---
+++ src/tokenizer.py
@@ -0,0 +1,42 @@
+import pickle
+from typing import Iterable, Iterator
+
+
+class BPETokenizer:
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens=None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens or []
+
+    @classmethod
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens=None):
+        """
+        Class method
+        that constructs and returns a Tokenizer from a serialized vocabulary and list of merges (in the
+        same format that your BPE training code output) and (optionally) a list of special tokens.
+        This method should accept the following additional parameters:
+        vocab_filepath: str
+        merges_filepath: str
+        special_tokens: list[str] | None = None
+        """
+        with open(vocab_filepath, "rb") as f:
+            vocab = pickle.load(f)
+        with open(merges_filepath, "rb") as f:
+            merges = pickle.load(f)
+        return cls(vocab, merges, special_tokens)
+
+    def encode(self, text: str) -> list[int]:
+        """Encode an input text into a sequence of token IDs."""
+        pass
+
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        """Given an iterable of strings (e.g., a Python file handle), return a generator that lazily yields token IDs. This is required for memory-efficient tokenization of large files that we cannot directly load into memory."""
+        pass
+
+    def decode(self, ids: list[int]) -> str:
+        """Decode a sequence of token IDs into text."""
+        pass
+
+    # To test your Tokenizer against our provided tests, you will first need to implement the test
+    # adapter at [adapters.get_tokenizer] . Then, run uv run pytest tests/test_tokenizer.py. Your
+    # implementation should be able to pass all tests.
